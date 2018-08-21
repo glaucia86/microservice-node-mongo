9e7a934000e449dfce1fb864f2d907eca610d1af
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-08-21 00:03:51 -0300

    Desenvolvimento do arquivo 'repository.js'

--- cinema-microservice/movies-service/src/repository/repository.js
+++ cinema-microservice/movies-service/src/repository/repository.js
@@ -4,4 +4,89 @@
  * Author: Glaucia Lemos
  * Description: Arquivo responsável por fazer a abstração da base de dados.
  * Date: 20/08/2018
+ * 
  */
+
+'use strict'
+
+// Função responsável por abrir a conexão de dados: 
+const repository = (db) => {
+    const collection = db.collection('movies');
+
+    // Método (GET): método responsável por retornar todos os filmes:
+    const getMoviePremiers = () => {
+        return new Promise((resolve, reject) => {
+            const movies = []
+            const currentDay = new Date();
+            const query = {
+                releaseYear: {
+                    $gt: currentDay.getFullYear() - 1,
+                    $lte: currentDay.getFullYear()
+                },
+                releaseMonth: {
+                    $gte: currentDay.getMonth() + 1,
+                    $lte: currentDay.getMonth() + 2
+                },
+                releaseDay: {
+                    $lte: currentDay.getDate()
+                }
+            }
+
+            const cursor = collection.find(query)
+            const addMovie = (movie) => {
+                movies.push(movie);
+            }
+
+            const sendMovies = (err) => {
+                if (error) {
+                    reject(new Error('Ocorreu um erro ao tentar listar os filmes...: ' + error));
+                }
+
+                resolve(movies);
+            }
+
+            cursor.forEach(addMovie, sendMovies);
+        });
+    }
+
+    // Método (GET): método responsável por retornar um determinado filme pelo Id:
+    const getMovieById = () => {
+        return new Promise((resolve, reject) => {
+            const projection = { _id: 0, id: 1, title: 1, format: 1 }
+            const sendMovie = (error, movie) => {
+                if (error) {
+                    reject(new Error(`Ocorreu um erro ao tentar retornar o filme de Id...: ${id}, error...: ${error}`));
+                }
+
+                resolve(movie);
+            }
+
+            collection.findOne({ id: id }, projection, sendMovie);
+        });
+    }
+
+    // Aqui iremos fechar a conexão com a Base de Dados:
+    const disconnect = () => {
+        db.close();
+    }
+
+    return Object.create({
+        getAllMovies,
+        getMoviePremiers,
+        getMovieById,
+        disconnect
+    });
+
+}
+
+const connect = (connection) => {
+    return new Promise((resolve, reject) => {
+        if (!connection) {
+            reject(new Error('Não conseguimos realizar a conexão com a base de dados!'));    
+        }
+
+        resolve(repository(connection));
+    });
+}
+
+module.exports = Object.assign({}, { connect });
