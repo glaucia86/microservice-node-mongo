0cd5a3d1a4c5ece6506aecc1f9e5a80c3c7a0b76
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-08-23 20:24:21 -0300

    Execução do comando 'lint' para colocar o código no padrão.

--- cinema-microservice/movies-service/src/api/movies.js
+++ cinema-microservice/movies-service/src/api/movies.js
@@ -7,29 +7,29 @@
 
 'use strict'
 
-const status = require('http-status');
+const status = require('http-status')
 
 module.exports = (app, options) => {
-    const { repo } = options
+  const { repo } = options
 
-    // Método (GET): método responsável por retornar todos os 'filmes':
-    app.get('/movies', (req, res, next) => {
-        repo.getAllMovies().then(movies => {
-            res.status(status.OK).json(movies);
-        }).catch(next)
-    })
+  // Método (GET): método responsável por retornar todos os 'filmes':
+  app.get('/movies', (req, res, next) => {
+    repo.getAllMovies().then(movies => {
+      res.status(status.OK).json(movies)
+    }).catch(next)
+  })
 
-    // Método (GET): método responsável por retornar somente os filmes de lançamento (premiere):
-    app.get('/movies/premieres', (req, res, next) => {
-        repo.getMoviePremiers().then(movies => {
-            res.status(status.OK).json(movies)
-        }).catch(next)
-    })
+  // Método (GET): método responsável por retornar somente os filmes de lançamento (premiere):
+  app.get('/movies/premieres', (req, res, next) => {
+    repo.getMoviePremiers().then(movies => {
+      res.status(status.OK).json(movies)
+    }).catch(next)
+  })
 
-    // Método (GET): método responsável por retornar um determinado filme por Id:
-    app.get('/movies/:id', (req, res, next) => {
-        repo.getMovieById().then(movie => {
-            res.status(status.OK).json(movie)
-        }).catch(next)
-    })
+  // Método (GET): método responsável por retornar um determinado filme por Id:
+  app.get('/movies/:id', (req, res, next) => {
+    repo.getMovieById().then(movie => {
+      res.status(status.OK).json(movie)
+    }).catch(next)
+  })
 }
