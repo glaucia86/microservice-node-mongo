07d389d3973247630c92696e9a1d25806602640a
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-08-23 21:23:21 -0300

    Alteração no arquivo seguindo o padrão do lint

--- cinema-microservice/movies-service/src/server/server.spec.js
+++ cinema-microservice/movies-service/src/server/server.spec.js
@@ -8,15 +8,15 @@
 const server = require('./server')
 
 describe('Teste: Server', () => {
-    it('Deve requerer a porta do servidor para iniciar a aplicação', () => {
-        return server.start({
-            repo: {}
-        }).should.be.rejectedWith(/port/)
-    })
+  it('Deve requerer a porta do servidor para iniciar a aplicação', () => {
+    return server.start({
+      repo: {}
+    }).should.be.rejectedWith(/port/)
+  })
 
-    it('Deve requerer um repositório para iniciar a aplicação', () => {
-        return server.start({
-            port: {}
-        }).should.be.rejectedWith(/repository/)
-    })
+  it('Deve requerer um repositório para iniciar a aplicação', () => {
+    return server.start({
+      port: {}
+    }).should.be.rejectedWith(/repository/)
+  })
 })
