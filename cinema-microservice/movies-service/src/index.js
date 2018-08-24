29b24b051bd6581b41c3fccbb7586cce93972ad2
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-08-23 22:16:01 -0300

    Desenvolvimento do arquivo 'server.js'

--- cinema-microservice/movies-service/src/index.js
+++ cinema-microservice/movies-service/src/index.js
@@ -4,3 +4,53 @@
  * Description: Arquivo responsável por levantar o servidor da aplicação via Node.js
  * Date: 20/08/2018
  */
+
+'use strict'
+
+const { EventEmmitter } = require('events')
+const server = require('./server/server')
+const repository = require('./repository/repository')
+const config = require('./config/config')
+
+const mediator = new EventEmmitter()
+
+console.log('---- Serviços dos Filmes ----')
+console.log('Conectando com os repositórios dos filmes....')
+
+process.on('uncaughtException', (error) => {
+    console.error('Ocorreu um erro de Exception aqui', error)
+})
+
+process.on('uncaughtException', (error, promise) => {
+    console.error('Ocorreu um erro de Rejection aqui', error)
+})
+
+mediator.on('db.ready', (db) => {
+    let rep
+    repository.connect(db)
+        .then(repo => {
+            console.log('Aplicação conectada ao repositório. Iniciando o serviço!')
+            rep = repo
+            return server.start({
+                port: config.serverSettings.port,
+                ssl: config.serverSettings.ssl,
+                repo
+            })
+        })
+        .then(app => {
+            console.log(`Aplicação conectada com sucesso. Executando na porta: ${config.serverSettings.port}.`)
+            app.on('close', () => {
+                rep.disconnect()
+            })    
+        })
+})
+
+mediator.on('db.error', (error) => {
+    console.log(error)
+})
+
+// Aqui vamos carregar a conexão para o repositório:
+config.db.connect(config.serverSettings, mediator)
+
+// E finalmente vamos iniciar a conexão com o repositório:
+mediator.emit('boot.ready')
