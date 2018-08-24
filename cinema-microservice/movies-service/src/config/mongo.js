0c762969ff883dae2680eda6231650541f71d076
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-08-23 21:44:14 -0300

    Alteração no arquivo 'mongo.js' seguindo os padrões do lint.

--- cinema-microservice/movies-service/src/config/mongo.js
+++ cinema-microservice/movies-service/src/config/mongo.js
@@ -9,32 +9,32 @@
 const MongoClient = require('mongodb')
 
 const getMongoUrl = (options) => {
-    const url = options.servers
-        .reduce((prev, cur) => prev + `${cur.ip}:${cur.port},`, 'mongodb://')
+  const url = options.servers
+    .reduce((prev, cur) => prev + `${cur.ip}:${cur.port},`, 'mongodb://')
 
-    return `${url.substr(0, url.length - 1)}/${options.db}`
+  return `${url.substr(0, url.length - 1)}/${options.db}`
 }
 
 // Aqui estaremos criando uma função para poder conectar, abrir e autenticar a conexão com o mongodb:
 const connect = (options, mediator) => {
-    mediator.once('boot.ready', () => {
-        MongoClient.connect(getMongoUrl(options), {
-            db: options.dbParameters(),
-            server: options.serverParameters(),
-            replset: options.replsetParameters(options.repl)
-        }, (error, db) => {
-            if (error) {
-                mediator.emit('db.error', error)
-            }
+  mediator.once('boot.ready', () => {
+    MongoClient.connect(getMongoUrl(options), {
+      db: options.dbParameters(),
+      server: options.serverParameters(),
+      replset: options.replsetParameters(options.repl)
+    }, (error, db) => {
+      if (error) {
+        mediator.emit('db.error', error)
+      }
 
-            db.admin().authenticate(options.user, options.pass, (error, result) => {
-                if (error) {
-                    mediator.emit('db.error', error)
-                }
-                mediator.emit('db.ready', db)
-            })
-        })
+      db.admin().authenticate(options.user, options.pass, (error, result) => {
+        if (error) {
+          mediator.emit('db.error', error)
+        }
+        mediator.emit('db.ready', db)
+      })
     })
+  })
 }
 
-module.exports = Object.assign({}, { connect})
+module.exports = Object.assign({}, { connect })
