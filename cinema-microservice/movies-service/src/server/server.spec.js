8ab72c83cbd04c7671449ee9109999f7592e8308
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-08-23 21:15:11 -0300

    Desenvolvimento do teste relacionado ao arquivo 'server.spec.js'

---
+++ cinema-microservice/movies-service/src/server/server.spec.js
@@ -0,0 +1,22 @@
+/**
+ * File: server.spec.js
+ * Author: Glaucia Lemos
+ * Description: Arquivo responsável por realizar testes relacionado ao arquivo '/server/server.js'
+ * Date: 21/08/2018
+*/
+
+const server = require('./server')
+
+describe('Teste: Server', () => {
+    it('Deve requerer a porta do servidor para iniciar a aplicação', () => {
+        return server.start({
+            repo: {}
+        }).should.be.rejectedWith(/port/)
+    })
+
+    it('Deve requerer um repositório para iniciar a aplicação', () => {
+        return server.start({
+            port: {}
+        }).should.be.rejectedWith(/repository/)
+    })
+})
